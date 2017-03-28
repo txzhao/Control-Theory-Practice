1894fd510bcf2d2a243f5fb4027138191d8dd295
Tianxiao Zhao <tzh@kth.se>
2017-03-28 13:51:08 +0200

    Update and rename 1 to basics.m

---
+++ lab 1/code/basics.m
@@ -0,0 +1,51 @@
+% initialize parameters
+pm = 30;
+wc = 0.4;
+r = 0;
+Ti = 1;     % for the case pm=50, Ti needs to increase
+
+% define G(s) and F_lag(s)
+s = tf('s');
+G = 3*(-s + 1)/((5*s + 1)*(10*s + 1));
+F_lag = (Ti*s + 1)/(Ti*s + r);
+
+% calculate required phase for lead compensator
+[~, p_lag] = bode(G*F_lag, wc);
+p_lead = pm - (p_lag - 180);
+
+% calculate parameters of lead compensator
+beta = (1 - sin(deg2rad(p_lead)))/(1 + sin(deg2rad(p_lead)));
+Td = 1/(wc*sqrt(beta));
+F_lead = (Td*s + 1)/(beta*Td*s + 1);
+K = 1/abs(evalfr(G*F_lag*F_lead, j*wc)); 
+
+% obtain lead-lag controller and closed-loop system
+F_lead = K*F_lead;
+F = F_lag*F_lead;
+sys = G*F/(1 + G*F);
+
+% plot result
+figure(1)
+margin(G)
+hold on
+margin(G*F)
+
+figure(2)
+step(sys)
+hold on
+step(G)
+
+% output system properties
+fb = bandwidth(sys);
+gpeak = getPeakGain(sys);
+S = stepinfo(sys);
+rt = S.RiseTime;
+overshoot = S.Overshoot;
+disp('=== system properties ===');
+disp(['Bandwidth: ' num2str(fb)]);
+disp(['Resonance Peak: ' num2str(gpeak)]);
+disp(['Rise Time: ' num2str(rt)]);
+disp(['Overshoot: ' num2str(overshoot) '%']);
+
+
+
